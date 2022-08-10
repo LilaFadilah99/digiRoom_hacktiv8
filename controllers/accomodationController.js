9d3891d783e437d21c0072be795c150f24e00c79
dila <lilafadila64@gmail.com>
2022-08-10 13:29:01 +0800

    add crud for accomodation endpoints

--- controllers/accomodationController.js
+++ controllers/accomodationController.js
@@ -1,9 +1,84 @@
+const e = require("express");
+const { Accomodation } = require("../models");
 class AccomodationController {
-  static async getAllAccomodation(request, response) {}
-  static async addAccomodation(request, response) {}
-  static async getAccomodationById(request, response) {}
-  static async updateAccomodation(request, response) {}
-  static async deleteAccomodation(request, response) {}
+  static async getAllAccomodation(request, response, next) {
+    try {
+      const accomodation = await Accomodation.findAll();
+      response.status(200).json(accomodation);
+    } catch (error) {
+      next(error);
+    }
+  }
+  static async addAccomodation(request, response, next) {
+    const { name, facility, roomCapacity, imgUrl, location, price, UserId, TypeId } = request.body;
+    try {
+      const newAccomodation = await Accomodation.create({
+        name,
+        facility,
+        roomCapacity,
+        imgUrl,
+        location,
+        price,
+        UserId,
+        TypeId,
+      });
+      response.status(201).json(newAccomodation);
+    } catch (error) {
+      next(error);
+    }
+  }
+  static async getAccomodationById(request, response, next) {
+    let { id } = request.params;
+    try {
+      const accomodationById = await Accomodation.findByPk(id);
+      if (!accomodationById) {
+        throw { name: "NotFound" };
+      } else {
+        response.status(200).json(accomodationById);
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+  static async updateAccomodation(request, response, next) {
+    let { id } = request.params;
+    let { name, facility, roomCapacity, imgUrl, location, price, UserId, TypeId } = request.body;
+    try {
+      const updateAccomodation = await Accomodation.update(
+        {
+          name,
+          facility,
+          roomCapacity,
+          imgUrl,
+          location,
+          price,
+          UserId,
+          TypeId,
+        },
+        { where: { id }, returning: true }
+      );
+      if (!updateAccomodation) {
+        throw { name: "NotFound" };
+      } else {
+        response.status(200).json(updateAccomodation);
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+  static async deleteAccomodation(request, response, next) {
+    let { id } = request.params;
+    try {
+      const deleteAccomodation = await Accomodation.destroy({ where: { id } });
+      if (!deleteAccomodation) {
+        throw { name: "NotFound" };
+      } else {
+        response.status(200).json(`success delete user id ${id}`);
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 module.exports = AccomodationController;
